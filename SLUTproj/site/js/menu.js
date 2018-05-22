7015c2bb574062b0387b1f195e68902eb74d60be
itggot-Amin-Othman <amin.othman@itggot.se>
2018-05-22 11:12:09 +0200

    ska börja på GRIDS

--- SLUTproj/site/js/menu.js
+++ SLUTproj/site/js/menu.js
@@ -1,9 +1,11 @@
 function openNav() {
     document.getElementById("myMenu").style.width = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
 }
 
 function closeNav() {
     document.getElementById("myMenu").style.width = "0";
     document.body.style.backgroundColor = "white";
+
 }
